0d5fcffd9c6f1282455b972a00ded6a089b8548d
BonnyCI <bonny@bonnyci.org>
2017-05-03 19:12:08 +0000

    Test commit
    bonnyci_test.tests.test_pull_requests.BonnyCIPRTests.test_pr_failed_gate_not_merged
    
     * .bonnyci/run.sh: Set: fail_gate=1 test_output=XXX TEST_ID: f3f7bb73-1380-4d28-97e9-4d21c99d585e fail_check=0

--- .bonnyci/run.sh
+++ .bonnyci/run.sh
@@ -1,6 +1,14 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-echo $BONNYCI_TEST_PIPELINE > $BONNYCI_TEST_LOG_DIR/pipeline
-dmesg >$BONNYCI_TEST_LOG_DIR/dmesg
-sleep 30
-echo "True"
+FAIL_CHECK=0
+FAIL_GATE=1
+TEST_OUTPUT="XXX TEST_ID: f3f7bb73-1380-4d28-97e9-4d21c99d585e"
+
+echo "$TEST_OUTPUT"
+
+case "$BONNYCI_TEST_PIPELINE" in
+    "check") [[ "$FAIL_CHECK" == 1 ]] && exit 1 ;;
+    "gate") [[ "$FAIL_GATE" == 1 ]] && exit 1 ;;
+esac
+
+exit 0
