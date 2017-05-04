f1ad47903be58cd19cee0e4143307a6515f8d2df
BonnyCI <bonny@bonnyci.org>
2017-05-04 19:00:40 +0000

    Test commit
    bonnyci_test.tests.test_pull_requests.BonnyCIPRTests.test_pr_failed_check_no_gate_job
    
     * .bonnyci/run.sh: Set: test_output=XXX TEST_ID: c893cf22-6cec-4232-ac96-fa67a47fab3c fail_gate=1 fail_check=1

--- .bonnyci/run.sh
+++ .bonnyci/run.sh
@@ -1,6 +1,14 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-echo $BONNYCI_TEST_PIPELINE > $BONNYCI_TEST_LOG_DIR/pipeline
-dmesg >$BONNYCI_TEST_LOG_DIR/dmesg
-sleep 30
-echo "True"
+FAIL_CHECK=1
+FAIL_GATE=1
+TEST_OUTPUT="XXX TEST_ID: c893cf22-6cec-4232-ac96-fa67a47fab3c"
+
+echo "$TEST_OUTPUT"
+
+case "$BONNYCI_TEST_PIPELINE" in
+    "check") [[ "$FAIL_CHECK" == 1 ]] && exit 1 ;;
+    "gate") [[ "$FAIL_GATE" == 1 ]] && exit 1 ;;
+esac
+
+exit 0
